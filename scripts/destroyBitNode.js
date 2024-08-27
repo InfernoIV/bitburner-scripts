15ec6cb3bbcb44610dbc619bffadc897e0729d97
InfernoIV <github@joostnieuweboer.nl>
2024-08-27 20:18:10 +0200

    Update destroyBitNode.js
    Added imports

--- scripts/destroyBitNode.js
+++ scripts/destroyBitNode.js
@@ -1,9 +1,14 @@
+//imports
+import { enum_scripts } from "scripts/common.js"
+
+
+
 /** 
  * @param {NS} ns 
  * Script that destroys the bitnode, defaults to bitnode 12 and boots with the boot script
  * This is a seperate script to save 20 GB RAM
  */
 export async function main(ns) {
-    //destroy bitnode
-    ns.singularity.destroyW0r1dD43m0n(12, "scripts/boot.js")
+    //destroy bitnode, go to bitnode 12, run boot script
+    ns.singularity.destroyW0r1dD43m0n(12, enum_scripts.boot)
 }
