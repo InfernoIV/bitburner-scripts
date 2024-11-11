12c4a71018843856e56fb9280fbbff39670ad675
Joost <github@joostnieuweboer.nl>
2024-11-11 13:03:56 +0100

    Update reset.js
    refactored
    	variables: 
    		augmentsBougt to augments_ready_for_install

--- scripts/reset.js
+++ scripts/reset.js
@@ -1,17 +1,27 @@
 //imports
-import { enum_scripts} from "scripts/common.js"
+import { enum_scripts } from "scripts/common.js"
 
 
-
-/** @param {NS} ns */
+/** 
+* Function that performs reset by installing augments or soft reset
+* Saves 10 GB by placing the reset outside of main loop
+* Costs: 15 GB
+* Singularity.softReset() = 5 GB
+* Singularity.installAugmentations() = 5 GB
+* Singularity.getOwnedAugmentations() = 5 GB
+* @param {NS} ns 
+*/
 export async function main(ns) {
-    const augmentsBougt = ns.singularity.getOwnedAugmentations(true).length - ns.singularity.getOwnedAugmentations(false).length
+    //get the augments bought and are ready for install
+    const augments_ready_for_install = ns.singularity.getOwnedAugmentations(true).length - ns.singularity.getOwnedAugmentations(false).length
     //if at least 1 augment bought
-    if (augmentsBougt > 0) {
+    if (augments_ready_for_install > 0) {
         //install augments
         ns.singularity.installAugmentations(enum_scripts.boot)
-        //otherwise soft reset
+        
+        //no augments bought
     } else {
+        //soft reset
         ns.singularity.softReset(enum_scripts.boot)
     }
 }
