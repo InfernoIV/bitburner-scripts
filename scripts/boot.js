3d16ba7b544c55903c143540a1769dbbafa58c0a
Joost <github@joostnieuweboer.nl>
2024-11-13 16:52:23 +0100

    Update boot.js
    Added information on bitnode multipliers

--- scripts/boot.js
+++ scripts/boot.js
@@ -66,3 +66,82 @@ export async function main(ns) {
         enum_scripts.main, true)
     } 
 }
+
+/**
+ * Function that logs information to console about the bitnode information
+*/
+function log_bit_node_information(bit_node_multipliers) {
+    for (const multiplier of bit_node_multipliers) {
+        //STUB
+    }
+}
+
+//Value that holds the comparison map, using https://github.com/bitburner-official/bitburner-src/blob/dev/src/BitNode/BitNodeMultipliers.ts
+//keys are the keys from getBitNodeMultipliers
+const bit_node_information = {
+    //Bitnode
+    WorldDaemonDifficulty: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the hacking skill required to backdoor the world daemon. */
+    DaedalusAugsRequirement: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how many Augmentations you need in order to get invited to the Daedalus faction */
+    CodingContractMoney: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the amount of money gained from completing Coding Contracts. */
+    ClassGymExpGain: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the experience gained for each ability when a player completes a class. */
+    InfiltrationMoney: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how much money is gained when the player infiltrates a company. */
+    InfiltrationRep: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how much rep the player can gain from factions when selling stolen documents and secrets */
+    //Stats
+    AgilityLevelMultiplier: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how quickly the player's agility level (not exp) scales */
+    DefenseLevelMultiplier: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how quickly the player's defense level (not exp) scales */
+    DexterityLevelMultiplier: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how quickly the player's dexterity level (not exp) scales */
+    HackExpGain: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the experienced gained when hacking a server. */
+    HackingLevelMultiplier: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how quickly the player's hacking level (not experience) scales */
+    HackingSpeedMultiplier: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how quickly the player's hack(), grow() and weaken() calls run */
+    StrengthLevelMultiplier: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how quickly the player's strength level (not exp) scales */
+    //Company
+    CompanyWorkExpGain: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the experience gained for each ability when the player completes working their job. */
+    CompanyWorkMoney: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how much money the player earns when completing working their job. */
+    CompanyWorkRepGain: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how much rep the player gains when performing work for a company. */
+    //Faction
+    FactionPassiveRepGain: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how much rep the player gains in each faction simply by being a member. */
+    FactionWorkExpGain: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the experience gained for each ability when the player completes work for a Faction. */
+    FactionWorkRepGain: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how much rep the player gains when performing work for a faction or donating to it. */
+    RepToDonateToFaction: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the minimum favor the player must have with a faction before they can donate to gain rep. */
+    //Augments
+    AugmentationMoneyCost: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the base cost to purchase an augmentation. */
+    AugmentationRepCost: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the base rep the player must have with a faction to purchase an augmentation. */
+    //Hacking
+    ManualHackMoney: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how much money can be stolen from a server when the player performs a hack against it through the Terminal */
+    ScriptHackMoney: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how much money can be stolen from a server when a script performs a hack against it. */
+    ScriptHackMoneyGain: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** The amount of money actually gained when a script hacks a server. This is different than the above because you can reduce the amount of money but not gain that same amount. */
+    ServerGrowthRate: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the growth percentage per cycle against a server. */
+    ServerMaxMoney: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the maximum money that a server can grow to. */
+    ServerStartingMoney: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the initial money that a server starts with. */
+    ServerStartingSecurity: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the initial security level (hackDifficulty) of a server. */
+    ServerWeakenRate: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the weaken amount per invocation against a server. */
+    //Servers
+    HacknetNodeMoney: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the hash rate of Hacknet Servers */
+    HomeComputerRamCost: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how much money it costs to upgrade your home computer's RAM */
+    PurchasedServerCost: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influence how much it costs to purchase a server */
+    PurchasedServerSoftcap: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influence how much it costs to purchase a server */
+    PurchasedServerLimit: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the maximum number of purchased servers you can have */
+    PurchasedServerMaxRam: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the maximum allowed RAM for a purchased server */
+    //Crime
+    CrimeExpGain: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the base experience gained for each ability when the player commits a crime. */
+    CrimeMoney: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the base money gained when the player commits a crime. */
+    CrimeSuccessRate: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the success chance of committing crimes */
+    //Bladeburner
+    BladeburnerRank: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how quickly the player can gain rank within Bladeburner. */
+    BladeburnerSkillCost: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the cost of skill levels from Bladeburner. */
+    //Gang
+    GangSoftcap: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the respect gain and money gain of your gang. */
+    GangUniqueAugs: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Percentage of unique augs that the gang has. */
+    //Corporation
+    CorporationDivisions: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the amount of divisions a corporation can have at the same time. */
+    CorporationSoftcap: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences profits from corporation dividends and selling shares. */
+    CorporationValuation: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the valuation of corporations created by the player. */
+    //Stocks
+    FourSigmaMarketDataApiCost: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how much it costs to unlock the stock market's 4S Market Data API */
+    FourSigmaMarketDataCost: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences how much it costs to unlock the stock market's 4S Market Data (NOT API) */
+    //Go
+    GoPower: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Percentage multiplier on the effect of the IPvGO rewards  */
+    //Stanek
+    StaneksGiftPowerMultiplier: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the power of the gift. */
+    StaneksGiftExtraSize: { default: 1, higher_is_better: false, disabled_value: 0, disabled_text: "" }, /** Influences the size of the gift. */
+}
