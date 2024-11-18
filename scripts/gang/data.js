a3bd7ad9f6aa296fff567e93d35b1e33e8716d5e
Joost <github@joostnieuweboer.nl>
2024-11-18 14:34:31 +0100

    Update data.js

--- scripts/gang/data.js
+++ scripts/gang/data.js
@@ -1,1 +1,15 @@
+//maximum number of gang members
+export const gang_members_max = 12 //gangMembersMax
 
+//tasks types to be used by functions
+export const gang_task = { //enumGangtask
+    unassigned: "unassigned",
+    lowerWanted: "lowerWanted",
+    trainHacking: "trainHacking",
+    trainCombat: "trainCombat",
+    trainCharisma: "trainCharisma",
+    power: "power",
+    //
+    reputation: "respect",
+    money: "money",
+}
