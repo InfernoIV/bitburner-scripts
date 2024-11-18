3e60b9f7e9c4f37b34948102e7c7082917d5cffb
Joost <github@joostnieuweboer.nl>
2024-11-18 14:21:51 +0100

    Create config.js

---
+++ scripts/hack/config.js
@@ -0,0 +1,6 @@
+//configurable delay in ms
+export const delay_between_scripts = 50
+//log level
+export const log_level = 0
+//define max amount of threads per instance
+export const max_threads = 100
