616059f0f7293708e99d93bc8a2bdbe438f66617
Joost <github@joostnieuweboer.nl>
2024-11-18 14:06:58 +0100

    Update config.js
    Added export tags

--- scripts/main/config.js
+++ scripts/main/config.js
@@ -1,12 +1,12 @@
 //minimum stat for hacking (todo: struct format?)
-const stat_minimum_hacking = 25//50 //?
+export const stat_minimum_hacking = 25//50 //?
 
 //set time to wait after each main loop
-const time_between_loops = 1 * 1000
+export const time_between_loops = 1 * 1000
 
 //minimum amount of augments before resetting
-const augments_minimum_for_reset = 4 //to be set to 40 for achievement
+export const augments_minimum_for_reset = 4 //to be set to 40 for achievement
 
 //target min chance before attempting bladeburner
-const bladeburner_success_chance_minimum = 1
-const bladeburner_black_op_success_chance_minimum = 0.5
+export const bladeburner_success_chance_minimum = 1
+export const bladeburner_black_op_success_chance_minimum = 0.5
