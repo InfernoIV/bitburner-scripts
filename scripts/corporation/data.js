24adbfe3076bcf271d3df79d006fb369adbc17fa
Joost <github@joostnieuweboer.nl>
2024-11-18 15:37:58 +0100

    Update data.js

--- scripts/corporation/data.js
+++ scripts/corporation/data.js
@@ -1,1 +1,93 @@
+//money needed to create corporation
+export const corporation_requirement_money = 150e9 //corporationMoneyRequirement
 
+//all cities
+const cities_all = ["Aevum", "Chongqing", "Sector-12", "New Tokyo", "Ishima", "Volhaven"] //citiesAll
+//main city to grow
+const city_main = "Aevum" //cityMain
+//cities that shoud support
+const city_support = ["Chongqing", "Sector-12", "New Tokyo", "Ishima", "Volhaven"] //citySupport
+
+//initial setup
+const division_material = "Agriculture" //divisionMaterial
+//main focus
+const division_product = "Tobacco" //divisionProduct
+
+//max productions
+const max_production = "MAX" //maxProduction
+//market price
+const market_price = "MP" //marketPrice
+
+//description of jobs
+const employee_jobs = { //employeeJob
+    operations: "Operations",
+    engineer: "Engineer",
+    business: "Business",
+    management: "Management",
+    randD: "Research & Development",
+    intern: "Intern",
+    unassigned: "Unassigned",
+}
+
+//description of upgrades
+const upgrade = {
+    smartFactories: "Smart Factories",
+    smartStorage: "Smart Storage",
+    dreamSense: "DreamSense",
+    wilsonAnalytics: "Wilson Analytics",
+    nuoptimalNootropicInjectorImplants: "Nuoptimal Nootropic Injector Implants",
+    speechProcessorImplants: "Speech Processor Implants",
+    neuralAccelerators: "Neural Accelerators",
+    focusWires: "FocusWires",
+    aBCSalesBots: "ABC SalesBots",
+    projectInsight: "Project Insight",
+}
+
+//description of unlocks
+const unlock = {
+    export: "Export",
+    smartSupply: "Smart Supply",
+    marketResearchDemand: "Market Research - Demand",
+    marketDataCompetition: "Market Data - Competition",
+    veChain: "VeChain",
+    shadyAccounting: "Shady Accounting",
+    governmentPartnership: "Government Partnership",
+    //warehouseAPI: "Warehouse API",
+    //officeAPI: "Office API",
+}
+
+//description of materials
+const material = {
+    water: "Water",
+    ore: "Ore",
+    minerals: "Minerals",
+    food: "Food",
+    plants: "Plants",
+    metal: "Metal",
+    hardware: "Hardware",
+    chemicals: "Chemicals",
+    drugs: "Drugs",
+    robots: "Robots",
+    aiCores: "AI Cores",
+    realEstate: "Real Estate",
+}
+
+//description of researches 
+const research = {
+    lab: "Hi-Tech R&D Laboratory",
+    autoBrew: "AutoBrew",
+    autoParty: "AutoPartyManager",
+    autoDrug: "Automatic Drug Administration",
+    cPH4Inject: "CPH4 Injections",
+    drones: "Drones",
+    dronesAssembly: "Drones - Assembly",
+    dronesTransport: "Drones - Transport",
+    goJuice: "Go-Juice",
+    recruitHR: "HRBuddy-Recruitment",
+    trainingHR: "HRBuddy-Training",
+    marketTa1: "Market-TA.I",
+    marketTa2: "Market-TA.II",
+    overclock: "Overclock",
+    selfCorrectAssemblers: "Self-Correcting Assemblers",
+    stimu: "Sti.mu",
+}
