30c631502cbafd636f96f46572e4bce61c65256d
Joost <github@joostnieuweboer.nl>
2024-11-18 14:39:12 +0100

    Update config.js

--- scripts/gang/config.js
+++ scripts/gang/config.js
@@ -1,1 +1,15 @@
+//log level
+export const log_level = -1
 
+//gang to join
+export const faction_gang = "Slum Snakes"
+
+//desired train level
+export const desired_training_Level = 200//50
+//desired multiplier before stopping training
+export const desired_multiplier = 10
+//desired equipment before continuing
+export const desired_equipment = 15//20 //24 takes too long... //32 total, 24 combat, 8 hacking
+
+//minimum percentage to start territory clashes (55%)
+export const territory_clash_minimum_percentage = 0.55 //territoryClashMinPercentage 
