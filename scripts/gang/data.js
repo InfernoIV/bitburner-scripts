2c7528e70597eedd886568172c8af7b85cbb02a5
Joost <github@joostnieuweboer.nl>
2024-11-18 14:39:33 +0100

    Update data.js
    Removed old names in comments

--- scripts/gang/data.js
+++ scripts/gang/data.js
@@ -1,8 +1,8 @@
 //maximum number of gang members
-export const gang_members_max = 12 //gangMembersMax
+export const gang_members_max = 12
 
 //tasks types to be used by functions
-export const gang_task = { //enumGangtask
+export const gang_task = { 
     unassigned: "unassigned",
     lowerWanted: "lowerWanted",
     trainHacking: "trainHacking",
