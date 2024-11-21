4017e9b7319d9b321deadd3c8358847611d906a3
Joost <github@joostnieuweboer.nl>
2024-11-21 17:38:36 +0100

    Update sleeve_dummy.js
    added stub functions

--- scripts/main/sleeve/sleeve_dummy.js
+++ scripts/main/sleeve/sleeve_dummy.js
@@ -1,3 +1,18 @@
 /*
 Dummy file to import when sleeve is not yet unlocked
 */
+export function init(ns) {
+  //stub
+}
+export function manage_actions(ns, bit_node_multipliers) {
+  //stub
+}
+export function buy_augments(ns) {
+  //stub
+}
+export function update_ui(ns) {
+  //stub
+}
+export function get_activity(ns, index = -1) {
+  //stub
+}
