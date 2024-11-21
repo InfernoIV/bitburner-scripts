7b7684e162587ebfbfde4f0b6e2fe373db72e8b7
Joost <github@joostnieuweboer.nl>
2024-11-21 11:29:53 +0100

    Update destroy_bit_node.js
    Moved determining of next target bit node to common.js

--- scripts/utility/destroy_bit_node.js
+++ scripts/utility/destroy_bit_node.js
@@ -1,7 +1,7 @@
 //imports
 import {    
     enum_scripts, //enums
-    get_bit_node_progression, get_reset_info, //functions
+    get_next_bit_node, //functions
 } from "scripts/common.js"
 
 
@@ -12,62 +12,8 @@ import {
  * This is a seperate script to save 20 GB RAM
  */
 export async function main(ns) {    
-    //get the current bit node information from file
-    const reset_info = get_reset_info(ns)
-    //get the map of owned source files (completed bitnodes) 
-    let owned_source_files_after_destruction = reset_info.ownedSF
-    
-    //check if we already have completed the current node
-    if (owned_source_files.has(reset_info.currentNode)) {
-        //get the level of the node
-        const bit_node_level = owned_source_files.get(reset_info.currentNode)
-        
-        //if bitnode 12
-        if (reset_info.currentNode == 12) {
-            //just add the counter
-            owned_source_files.set(reset_info.currentNode, bit_node_level + 1)
-        
-        //if not maxxed
-        } else if (bit_node_level < 3) {
-            //add the level
-            owned_source_files.set(reset_info.currentNode, bit_node_level + 1)
-        }
-        
-    //otherwise add it to the map (this should be the overview AFTER the bitnode
-    } else {
-        //add the bitnode to the source file list at level 1 (first completion)
-        owned_source_files.set(reset_info.currentNode, 1)
-    }
-
-    //default target bit node 12, since it is endless
-    let next_bit_node = 12
-    //for every step in the planned progression
-    for (const step of get_bit_node_progression(ns)) {    //bit_node_progression = [ { bit_node: 1, level: 1 } ]
-        //if we have completed the bit node (and thus it is present in the map)
-        if (owned_source_files.has(step.bit_node)) {
-            
-            //if we have the required level
-            if(owned_source_files.get(step.bit_node) == step.level) {
-                //everything is in order, go to next
-                continue
-                
-            //we do not have the required level
-            } else {
-                //set the target to this bit node
-                next_bit_node = step.bit_node
-                //stop searching
-                break
-            }
-            
-        //bitnode has not been tackled yet
-        } else {
-            //set the target to this bit node
-            next_bit_node = step.bit_node
-            //stop searching
-            break
-        }
-    }
-    
+    //determine next bit node
+    const next_bit_node = get_next_bit_node(ns)
     //destroy bitnode, go to targeted bitnode, run boot script
     ns.singularity.destroyW0r1dD43m0n(next_bit_node, enum_scripts.boot)
 }
