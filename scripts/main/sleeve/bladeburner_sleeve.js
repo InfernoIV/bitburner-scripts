fc7668976dc76020deff0b56d1fe22da496507bb
Joost <github@joostnieuweboer.nl>
2024-11-26 14:27:33 +0100

    Update bladeburner_sleeve.js

--- scripts/main/sleeve/bladeburner_sleeve.js
+++ scripts/main/sleeve/bladeburner_sleeve.js
@@ -1,53 +1,55 @@
-//config
-import * as config from "./config.js"
-//data
-import * as data from "./data.js"
-//bladeburner
-import * as bladeburner from "../bladeburner/data.js"
-
-
+//bladeburner config
+import * as config from "../bladeburner/config.js"
+//bladeburner data
+import * as data from "../bladeburner/data.js"
+//bladeburner functions
+import * as bladeburner from "../bladeburner/bladeburner.js"
 
 
 
 /**
- * Function that checks the best work type for bladeburner for sleeve
+ * Function that checks the best work type for bladeburner for sleeve, returns it in order of priority
+ * returns empty list if no work is applicable
  * Only perform 100% chance work, else we get shock
- * cannot lower chaos, requires to get city, which increases ram usage
+ * cannot lower chaos, requires to get city, which increases ram usage -> just selected to lowest chaos city and check threshold
  * @param {NS} ns
  * Cost: 0 GB
  */
-function getBladeburnerActionForSleeve(ns, sleeveIndex) {
-
-    /*
-    //keep track of action count
-    let bladeburner_total_action_count = 0
-
-    //contracts
-    //for each contract
-    for (const operation in enum_bladeburnerActions.contracts) {
-        //get contract information
-        const contract = enum_bladeburnerActions.contracts[operation]
-        //get action count
-        const action_count = ns.bladeburner.getaction_countRemaining(enum_bladeburnerActions.type.contracts, contract)
-        //get chance
-        const chance = ns.bladeburner.getActionEstimatedSuccessChance(enum_bladeburnerActions.type.contracts, contract, sleeveIndex)
-        //if this action can be performed and we have enough chance
-        if ((action_count > 0) && (chance[0] >= config.bladeburner_success_chance_minimum)) {
-            //return this information
-            return { type: enum_sleeveBladeburnerActions.takeonContracts, name: contract }
-        }
-        //add count to total actions available
-        bladeburner_total_action_count += action_count
-    }
-
-    //if no operations or contracts available
-    if (bladeburner_total_action_count == 0) {
-        //set to create contracts
-        return { type: enum_bladeburnerActions.type.general, name: enum_bladeburnerActions.general.inciteViolence }
-    }
-    */
-    //failsafe: just train
-    return { type: enum_bladeburnerActions.type.general, name: enum_bladeburnerActions.general.training }
+function bladeburner_get_actions_sleeve(ns, sleeveIndex) {
+    //create a variable to store actions into that are wanted
+    let action_list_wanted = []
+    //create a variable to store actions into that are possible (enough chance)
+    let action_list_possible = []
     
+    //if available actions are getting too low 
+    if (bladeburner_get_lowest_action_count(ns) <= config.bladeburner_minimum_number_of_actions) {
+        //if enough chance
+        
+        //add to the list
+        action_list_wanted.push({ type: data.bladeburner_actions.sleeve.infiltrate_synthoids, name: true })
+    }
+    
+    //lower city chaos (if needed)
+    if (ns.bladeburner.getCityChaos(bladeburner_get_best_city(ns)) > config.bladeburner_chaos_threshold) {
+        //lower chaos
+        action_list_wanted.push({ type: data.bladeburner_actions.sleeve.diplomacy, name: true })
+    }
+    
+    //recruit members
+    action_list_wanted.push({ type: data.bladeburner_actions.sleeve.recruitment, name: true })
+    
+    //for each action in the list (from most important to least important)
+    for (const action in action_list_wanted) {
+        //get chance
+        const chance = ns.bladeburner.getActionEstimatedSuccessChance(action.type, sleeveIndex)
+        //if enough chance
+        if (chance >= config.bladeburner_success_chance_minimum_sleeve) {
+            //add to the list of possible actions
+            action_list_possible.push(action)
+        }
+    }
+    
+    //return the list of possible actions
+    return action_list_possible
 }
 
