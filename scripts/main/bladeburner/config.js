dae949d67d05a8dee56361c39fffeb2ec1300ca1
Joost <github@joostnieuweboer.nl>
2024-11-26 13:58:53 +0100

    Create config.js

---
+++ scripts/main/bladeburner/config.js
@@ -0,0 +1,7 @@
+//success chance for blade burner actions for player
+const bladeburner_success_chance_minimum_player = 1
+
+//success chance for blade burner actions for sleeve
+const bladeburner_success_chance_minimum_sleeve = 1
+//minimum number of actions before generating more number of actions
+const bladeburner_minimum_number_of_actions = 2
