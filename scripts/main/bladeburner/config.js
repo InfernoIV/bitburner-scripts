6a585019f4665ad4b945d3d5d5e20037e12153de
Joost <github@joostnieuweboer.nl>
2024-11-26 14:26:14 +0100

    Update config.js
    Replace , by .

--- scripts/main/bladeburner/config.js
+++ scripts/main/bladeburner/config.js
@@ -1,7 +1,7 @@
 import * as data from "./data.js"
 
 //threshold on which chaos needs to be reduces (preventing it from impacting
-export const bladeburner_chaos_threshold = data.bladeburner_chaos_value_for_penalty * 0,9 //90%
+export const bladeburner_chaos_threshold = data.bladeburner_chaos_value_for_penalty * 0.9 //90%
 
 //success chance for blade burner actions for player
 const bladeburner_success_chance_minimum_player = 1
