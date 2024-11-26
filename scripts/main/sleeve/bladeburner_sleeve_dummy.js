1e98dec022d5412378be68e621ae17d6b727b551
Joost <github@joostnieuweboer.nl>
2024-11-26 15:31:59 +0100

    Update bladeburner_sleeve_dummy.js

--- scripts/main/sleeve/bladeburner_sleeve_dummy.js
+++ scripts/main/sleeve/bladeburner_sleeve_dummy.js
@@ -1,1 +1,3 @@
-//stub
+//dummy file to return empty functions (lowering RAM costs when functionality is not yet unlocked)
+export function bladeburner_manage_actions(ns, sleeve_actions) { }
+export function bladeburner_execute_actions(ns, sleeve_actions) { }
