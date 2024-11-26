74f0d280d0afea1016a5eb3003858b1be3af49e3
Joost <github@joostnieuweboer.nl>
2024-11-26 13:53:36 +0100

    Update data.js
    added sleeve actions to bladeburner actions object

--- scripts/main/bladeburner/data.js
+++ scripts/main/bladeburner/data.js
@@ -78,6 +78,20 @@ export const bladeburner_actions = {
         hyperbolicRegen: "Hyperbolic Regeneration Chamber",
         inciteViolence: "Incite Violence",
     },
+    //available sleeve actions, split just for clarity
+    //https://github.com/bitburner-official/bitburner-src/blob/dev/src/PersonObjects/Sleeve/Sleeve.ts#L479
+    sleeve: {
+        //general actions
+        training: "Training",
+        recruitment: "Recruitment",
+        field_analysis: "Field Analysis",
+        diplomacy: "Diplomacy",
+        hyperbolic_regeneration_chamber: "Hyperbolic Regeneration Chamber",
+        //sleeve unique actions
+        infiltrate_synthoids: "Infiltrate synthoids", //add action count to operations and contracts
+        support_main_sleeve: "Support main sleeve", //become part of team, like a team member
+        take_on_contracts: "Take on contracts", //perform contract work
+    },
 }
 
 
