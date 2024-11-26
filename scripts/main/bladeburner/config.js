aa8637843438b9f131fd3dab9b5437b9ba940f20
Joost <github@joostnieuweboer.nl>
2024-11-26 14:29:43 +0100

    Update config.js

--- scripts/main/bladeburner/config.js
+++ scripts/main/bladeburner/config.js
@@ -1,12 +1,14 @@
+//data
 import * as data from "./data.js"
 
 //threshold on which chaos needs to be reduces (preventing it from impacting
 export const bladeburner_chaos_threshold = data.bladeburner_chaos_value_for_penalty * 0.9 //90%
 
 //success chance for blade burner actions for player
-const bladeburner_success_chance_minimum_player = 1
+export const bladeburner_success_chance_minimum_player = 1
 
 //success chance for blade burner actions for sleeve
-const bladeburner_success_chance_minimum_sleeve = 1
+export const bladeburner_success_chance_minimum_sleeve = 1
+
 //minimum number of actions before generating more number of actions
-const bladeburner_minimum_number_of_actions = 2
+export const bladeburner_minimum_number_of_actions = 2
