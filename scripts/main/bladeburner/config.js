28d4a5b3cfef8fe38e3703a4e3de7bf5f2720278
Joost <github@joostnieuweboer.nl>
2024-11-26 14:25:58 +0100

    Update config.js
    added config for chaos threshold

--- scripts/main/bladeburner/config.js
+++ scripts/main/bladeburner/config.js
@@ -1,3 +1,8 @@
+import * as data from "./data.js"
+
+//threshold on which chaos needs to be reduces (preventing it from impacting
+export const bladeburner_chaos_threshold = data.bladeburner_chaos_value_for_penalty * 0,9 //90%
+
 //success chance for blade burner actions for player
 const bladeburner_success_chance_minimum_player = 1
 
