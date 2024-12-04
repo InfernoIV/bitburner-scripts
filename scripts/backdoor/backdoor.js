641b4405c9f39c2bcecd5f9da638bdb089bfa052
Joost <github@joostnieuweboer.nl>
2024-12-04 09:56:08 +0100

    Update backdoor.js
    Added config import
    Moved common import to * import instead of specifics

--- scripts/backdoor/backdoor.js
+++ scripts/backdoor/backdoor.js
@@ -1,9 +1,7 @@
-//imports
-import {  
-    success, error, portNoData, //constants
-    enum_port, enum_servers, //enums
-    log, //functions
-} from "scripts/common.js"
+//import common functions
+import * as common from "./common.js"
+//import config
+import * as config from "/config.js"
 
 
 
@@ -12,51 +10,16 @@ export async function main(ns) {
     //initialize
     init(ns)
     
-    //connect to home
-    ns.singularity.connect(enum_servers.home)
-
     //main loop
     while (true) {
-        //get hostname
+        //get hostname (only remove data when finished)
         let server = ns.peek(enum_port.backdoor)
         //if set
-        if (server != portNoData) {
-            //create a list to hold the route
-            let route = []
-            //create a variable to save current server, and set it to current hostname
-            let step = server
-            //while not found home
-            while (step != enum_servers.home) {
-                //save the first scan result
-                let nextStep = ns.scan(step)[0]
-                //add current to the start of the list
-                route.unshift(step)
-                //update target for next scan
-                step = nextStep
-            }
-            
-            //for every jump of the route    
-            for (let jump of route) {
-                //connect to the step
-                ns.singularity.connect(jump)
-            }
-            
-            //try-catch to ensure script not crashing
-            try {
-                //install backdoor
-                await ns.singularity.installBackdoor()
-                //log locally
-                log(ns, 0, success, "Backdoored server '" + server + "'")
-                //catch error
-            } catch (err) {
-                //log error
-                log(ns, 0, error, "Failed to backdoored server '" + server + "': " + err)
-            }
-            //connect to home
-            ns.singularity.connect(enum_servers.home)
-
-            //remove the current hostname from the port
-            ns.readPort(enum_port.backdoor)
+        if (server != common.portNoData) {
+            //backdoor the server
+            await backdoor_server(ns, server)
+            //remove the current hostname from the port (only when it has finished
+            ns.readPort(common.port.backdoor)
         }
         //wait a bit
         await ns.sleep(100)
@@ -75,4 +38,47 @@ function init(ns) {
     ns.disableLog("scan")
     ns.disableLog("singularity.installBackdoor")
     ns.disableLog("sleep")
+    //connect to home
+    ns.singularity.connect(enum_servers.home)
+}
+
+
+
+/**
+ * Function that checks on port 
+**/
+function async backdoor_server(ns, server) {
+    //create a list to hold the route
+    let route = []
+    //create a variable to save current server, and set it to current hostname
+    let step = server
+    //while not found home
+    while (step != common.servers.home) {
+        //save the first scan result
+        let nextStep = ns.scan(step)[0]
+        //add current to the start of the list
+        route.unshift(step)
+        //update target for next scan
+        step = nextStep
+    }
+    
+    //for every jump of the route    
+    for (let jump of route) {
+        //connect to the step
+        ns.singularity.connect(jump)
+    }
+    
+    //try-catch to ensure script not crashing
+    try {
+        //install backdoor
+        await ns.singularity.installBackdoor()
+        //log information
+        common.log(ns, config.log_level, common.success, "Backdoored server '" + server + "'")
+        //catch error
+    } catch (err) {
+        //log error
+        common.log(ns, config.log_level, common.error, "Failed to backdoored server '" + server + "': " + err)
+    }
+    //connect to home
+    ns.singularity.connect(common.servers.home)
 }
