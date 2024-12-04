4e98d60125910230364314a623a2552f953bd549
Joost <github@joostnieuweboer.nl>
2024-12-04 14:21:50 +0100

    Update config.js
    Added 2 variables, changed variable names

--- scripts/gang/config.js
+++ scripts/gang/config.js
@@ -1,15 +1,20 @@
+//data
+import * as data from "./data.js"
+
 //log level
 export const log_level = -1
 
-//gang to join
-export const faction_gang = "Slum Snakes"
-
-//desired train level
+//desired train level (unlimited)
 export const desired_training_Level = 200//50
-//desired multiplier before stopping training
+//desired multiplier before stopping training (unlimited)
 export const desired_multiplier = 10
-//desired equipment before continuing
-export const desired_equipment = 15//20 //24 takes too long... //32 total, 24 combat, 8 hacking
+//desired equipment before continuing (max 32: 24 combat and 8 hacking)
+export const desired_equipment = 15//20 //24 takes too long...
 
-//minimum percentage to start territory clashes (55%)
-export const territory_clash_minimum_percentage = 0.55 //territoryClashMinPercentage 
+//minimum percentage to start territory clashes 
+export const territory_clash_minimum_percentage = 0.55 //55%
+
+//focus of the gang
+export const focus = data.focus_area.combat
+//gang to join
+export const faction_gang = data.factions.slum_snakes
