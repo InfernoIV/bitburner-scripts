d1540365c3c2ad6b28ab610d0e21b4390c223097
Joost <github@joostnieuweboer.nl>
2024-12-04 11:36:02 +0100

    Update sleeve_dummy.js
    added scaling parameters and added explanation and return statements to the dummy functions

--- scripts/main/sleeve/sleeve_dummy.js
+++ scripts/main/sleeve/sleeve_dummy.js
@@ -1,6 +1,5 @@
 //dummy file to return empty functions (lowering RAM costs when functionality is not yet unlocked)
-export function init(ns) { }
-export function manage_actions(ns, bit_node_multipliers) { }
-export function buy_augments(ns) { }
-export function update_ui(ns) { }
-export function get_activity(ns, index = -1) { }
+export function init(...parameters) { /* do nothing */ }
+export function manage_actions(...parameters) { /* do nothing */ }
+export function buy_augments(...parameters) { /* do nothing */ }
+export function update_ui(...parameters) { return [[],[]] /* return empty list */ }
