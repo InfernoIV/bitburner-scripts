a81459ddb2407f1a621c10973f8fc8d2ba61a706
Joost <github@joostnieuweboer.nl>
2024-12-05 15:10:15 +0100

    Update data.js
    Refactored gang_task names, added more gang factions

--- scripts/gang/data.js
+++ scripts/gang/data.js
@@ -1,13 +1,16 @@
+//common
+import * as common from "./common.js"
+
 //maximum number of gang members
 export const members_max = 12
 
 //tasks types to be used by functions
 export const gang_task = { 
     unassigned: "unassigned",
-    lowerWanted: "lowerWanted",
-    trainHacking: "trainHacking",
-    trainCombat: "trainCombat",
-    trainCharisma: "trainCharisma",
+    lower_wanted: "lowerWanted",
+    train_hacking: "trainHacking",
+    train_combat: "trainCombat",
+    train_charisma: "trainCharisma",
     power: "power",
     //
     reputation: "respect",
@@ -22,5 +25,13 @@ export const focus_area = {
 
 //available factions that can be joined as a gang
 export const factions = {
-    slum_snakes: "Slum Snakes",
+    //combaat focussed
+    slum_snakes: common.factions.slum_snakes.name,
+    tetrads: common.factions.tetrads.name,
+    the_syndicate: common.factions.the_syndicate.name,
+    the_dark_army: common.factions.the_dark_army.name,
+    speakers_for_the_dead: common.factions.speakers_for_the_dead.name,
+    //hacking focussed
+    nite_sec: common.factions.nite_sec.name,
+    the_black_hand: common.factions.the_black_hand.name,
 }
