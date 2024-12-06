2994468be96dc567129002fc3dfd6a9074eafb38
Joost <github@joostnieuweboer.nl>
2024-12-06 16:13:51 +0100

    Update common.js
    Added functions disable_logging

--- scripts/common.js
+++ scripts/common.js
@@ -559,3 +559,16 @@ export function get_availability_functionality(ns, functionality) {
   return false
 }
 
+
+
+/**
+ * 
+ */
+export function disable_logging(ns, log_topics) {
+  //assuming array of log topics
+  //for each topic
+  for (const log_topic of log_topics) {
+    //disable logging
+    ns.disableLog(log_topic)
+  }
+}
