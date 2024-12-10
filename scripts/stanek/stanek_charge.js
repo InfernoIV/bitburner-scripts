1cab9a67438e8cbd1ad4fa570183ecc94933d1cd
Joost <github@joostnieuweboer.nl>
2024-12-10 14:39:07 +0100

    Update stanek_charge.js
    Refactored variable names

--- scripts/stanek/stanek_charge.js
+++ scripts/stanek/stanek_charge.js
@@ -1,9 +1,5 @@
-//imports
-import { 
-  enum_port, enum_servers, enum_scripts,
-  log, info, success, warning, error, fail,
-} from "scripts/common.js"
-
+//common
+import * as common from "./common.js"
 //config
 import * as config from "./config.js"
 //data
@@ -22,21 +18,21 @@ export async function main(ns) {
         ns.killall(enum_servers.home, true)  
       
         //save biggest RAM
-        const maxRam = ns.getServerMaxRam(enum_servers.home) - ns.getScriptRam(enum_scripts.stanekCharge, enum_servers.home)
+        const max_ram = ns.getServerMaxRam(enum_servers.home) - ns.getScriptRam(enum_scripts.stanekCharge, enum_servers.home)
         //calculate RAM cost
-        const scriptRam = ns.getScriptRam(enum_scripts.workerCharge, enum_servers.home)
+        const script_ram = ns.getScriptRam(enum_scripts.workerCharge, enum_servers.home)
         //check how many threads we can run
-        const threads = Math.floor(maxRam / scriptRam)
+        const threads = Math.floor(max_ram / script_ram)
 
         //log start charging
-        log(ns, 1, info, "Charging " + ns.stanek.activeFragments().length + " fragments " + config.number_of_charges + "x")
+        common.log(ns,config.log_level, common.info, "Charging " + ns.stanek.activeFragments().length + " fragments " + config.number_of_charges + "x")
         //charge multiple times
         for (let index = 0; index < config.number_of_charges; index++) {
             //charge the fragments
             await chargeFragments(ns, threads)
         }
         //log completion
-        log(ns, 1, success, "Charging fragments complete!")
+        common.log(ns, config.log_level, common.success, "Charging fragments complete!")
 
         //after charging, rep for CotMG increases: check if we can buy augments
         for (const augment of ns.singularity.getAugmentationsFromFaction(data.faction)) {
@@ -46,7 +42,8 @@ export async function main(ns) {
     }
 
     //run jumpscript to boot main
-    ns.run(enum_scripts.jump, 1, enum_scripts.main)
+    ns.run(common.scripts.jump, 1, 
+           common.scripts.main)
 }
 
 
@@ -56,15 +53,7 @@ export async function main(ns) {
  */
 function init(ns) {
     //disable logs
-    ns.disableLog("disableLog")
-    ns.disableLog("sleep")
-    ns.disableLog("killall")
-    ns.disableLog("scp")
-    ns.disableLog("stanek.activeFragments")
-    ns.disableLog("scan")
-    //ns.disableLog("exec")
-    //ns.disableLog("run")
-    ns.disableLog("getServerMaxRam")
+    common.disable_logs(ns, log_disabled_topics)
 }
 
 
@@ -74,18 +63,18 @@ function init(ns) {
  */
 async function chargeFragments(ns, threads) {
     //for each active fragment
-    for (const fragmentIndex in ns.stanek.activeFragments()) {
+    for (const fragment_index in ns.stanek.activeFragments()) {
         //get fragment
-        const fragment = ns.stanek.activeFragments()[fragmentIndex]
+        const fragment = ns.stanek.activeFragments()[fragment_index]
         //check for booster fragments (cannot be charged)
         if (fragment.id < data.fragment_booster_id_start) {
             //charge the fragment
-            ns.run(enum_scripts.workerCharge, threads,
+            ns.run(common.scripts.worker_charge, threads,
                 //arguments: root x, root y, amount of charges
                 fragment.x, fragment.y)
             //wait for the script to complete
             //while not getting the "Done" message
-            while (ns.readPort(enum_port.stanek) != config.message_done) {
+            while (ns.readPort(common.port.stanek) != config.message_done) {
                 //wait a bit
                 await ns.sleep(1)
             }
