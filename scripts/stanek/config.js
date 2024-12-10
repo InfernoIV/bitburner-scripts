525385ba2021c3f1292bd149eade06f5c2d67bf3
Joost <github@joostnieuweboer.nl>
2024-12-10 14:36:43 +0100

    Update config.js
    Added log_disabled_topics

--- scripts/stanek/config.js
+++ scripts/stanek/config.js
@@ -2,5 +2,17 @@
 export const number_of_charges = 10
 //define message to indicate fininsh
 export const message_done = "Done"
+
 //log level
 export const log_level = 0
+//disabled log topics
+export const log_disabled_topics = [
+    "sleep",
+    "killall",
+    "scp",
+    "stanek.activeFragments",
+    "scan",
+    //"exec",
+    //"run",
+    "getServerMaxRam",
+]
