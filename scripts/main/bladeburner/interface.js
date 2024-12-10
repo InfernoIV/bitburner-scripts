1da72793c9d90529bc01c68df11c65b2785ae91d
Joost <github@joostnieuweboer.nl>
2024-12-10 14:22:14 +0100

    Update interface.js
    Fixed import paths

--- scripts/main/bladeburner/interface.js
+++ scripts/main/bladeburner/interface.js
@@ -2,8 +2,8 @@
 import * as common from "./common.js"
 
 //import the the needed namespaces
-import bladeburner_dummy from "./bladeburner_dummy.js"
-import bladeburner from "./bladeburner.js"
+import bladeburner_dummy from "bladeburner_dummy.js"
+import bladeburner from "bladeburner.js"
 
 //check which one to export (depending on availability)
 const exported_namespace = common.functionality_available(common.functionality.bladeburner) ? bladeburner : bladeburner_dummy
