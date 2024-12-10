f3d7bef7f91d5072547ff81afae8eba6a4c44764
Joost <github@joostnieuweboer.nl>
2024-12-10 22:26:25 +0100

    Update jump_script.js
    Converter import to * import

--- scripts/utility/jump_script.js
+++ scripts/utility/jump_script.js
@@ -1,13 +1,10 @@
-import { 
-  enum_scripts, enum_servers,
-  log, info, success, warning, error, fail,
-} from "scripts/common.js"
-
+//common
+import * as common from "./common.js"
 
 /** @param {NS} ns */
 export async function main(ns) {
     let script = ""
-    let killAll = false
+    let kill_all = false
     let parameters = []
     //ns.tprint("args: " + JSON.stringify(ns.args))
 
@@ -17,28 +14,29 @@ export async function main(ns) {
 
         if (ns.args.length > 1) {
             //2nd arg is to kill all other scripts (optional)
-            killAll = ns.args[1]
+            kill_all = ns.args[1]
         }
         //if kill all other scripts
-        if (killAll) {
-            ns.killall(enum_servers.home, true)
+        if (kill_all) {
+            ns.kill_all(common.servers.home, true)
         }
 
         if (ns.args.length > 2) {
             //3+ args is to pass on to target script
             for (let arg of ns.args.slice(2)) {
+                //add the arguments
                 parameters.push(arg)
             }
             const arg1 = ns.args[2]
             const arg2 = ns.args[3]
             //start script on on home
-            ns.run(enum_scripts.jump2, 1, script, arg1, arg2)
+            ns.run(common.scripts.jump2, 1, script, arg1, arg2)
         } else {
             //start script on on home
-            ns.run(enum_scripts.jump2, 1, script)
+            ns.run(common.scripts.jump2, 1, script)
 
         }
     } catch (err) {
-        log(ns, 1, error, err)
+        common.log(ns, 1, common.error, err)
     }
 }
