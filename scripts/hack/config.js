604c911d959b5bac478c7b27617ce411b9d80476
Joost <github@joostnieuweboer.nl>
2024-12-10 09:52:49 +0100

    Update config.js
    added log_disabled_topics

--- scripts/hack/config.js
+++ scripts/hack/config.js
@@ -2,5 +2,21 @@
 export const delay_between_scripts = 50
 //log level
 export const log_level = 0
+//disabled logs
+export const log_disabled_topics = [
+    "getServerMaxRam",
+    "getServerUsedRam",
+    "sleep",
+    "getServerMoneyAvailable",
+    "exec",
+    "getServerSecurityLevel",
+    "getServerRequiredHackingLevel",
+    "getServerMinSecurityLevel",
+    "getServerMaxMoney",
+    "getHackingLevel",
+    "scan",
+    "getServerBaseSecurityLevel",
+    "scp",
+]
 //define max amount of threads per instance
 export const max_threads = 100
