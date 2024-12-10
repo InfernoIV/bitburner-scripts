6032c9e31eb9c68bad883de3dc0a8b8a06584749
Joost <github@joostnieuweboer.nl>
2024-12-10 14:21:57 +0100

    Update interface.js
    Fixed import paths

--- scripts/main/hacknet/interface.js
+++ scripts/main/hacknet/interface.js
@@ -2,8 +2,8 @@
 import * as common from "./common.js"
 
 //import the the needed namespaces
-import servers from "./servers.js"
-import hacknet from "./hacknet.js"
+import servers from "servers.js"
+import hacknet from "hacknet.js"
 
 //check which one to export (depending on availability)
 const exported_namespace = common.functionality_available(common.functionality.hacknet) ? hacknet : servers
