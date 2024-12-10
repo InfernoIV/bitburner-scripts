548bd0b2be80b40868bcbc73eb9602794af54776
Joost <github@joostnieuweboer.nl>
2024-12-10 15:32:38 +0100

    Update bladeburner.js
    imported commented code from main.js

--- scripts/main/sleeve/bladeburner/bladeburner.js
+++ scripts/main/sleeve/bladeburner/bladeburner.js
@@ -79,3 +79,53 @@ export function bladeburner_execute_actions(ns, sleeve_actions) {
         }
     }
 }
+
+
+/**
+ * Function that checks the best work type for bladeburner for sleeve
+ * Only perform 100% chance work, else we get shock
+ * cannot lower chaos, requires to get city, which increases ram usage
+ * @param {NS} ns
+ * Cost: 0 GB
+ */
+    /*
+function getBladeburnerActionForSleeve(ns, sleeveIndex) {
+    //set min chance = 100%
+    const bladeburner_success_chance_minimum = 1
+    //describe other actions
+    const common.sleeveBladeburnerActions = {
+        fieldAnalysis: "Field Analysis",
+        infiltratesynthoids: "Infiltrate synthoids",
+    }
+
+    //keep track of action count
+    let bladeburner_total_action_count = 0
+
+    //contracts
+    //for each contract
+    for (const operation in common.bladeburnerActions.contracts) {
+        //get contract information
+        const contract = common.bladeburnerActions.contracts[operation]
+        //get action count
+        const action_count = ns.bladeburner.getActionCountRemaining(common.bladeburnerActions.type.contracts, contract)
+        //get chance
+        const chance = ns.bladeburner.getActionEstimatedSuccessChance(common.bladeburnerActions.type.contracts, contract, sleeveIndex)
+        //if this action can be performed and we have enough chance
+        if ((action_count > 0) && (chance[0] >= bladeburner_success_chance_minimum)) {
+            //return this information
+            return { type: common.sleeveBladeburnerActions.takeonContracts, name: contract }
+        }
+        //add count to total actions available
+        bladeburner_total_action_count += action_count
+    }
+
+    //if no operations or contracts available
+    if (bladeburner_total_action_count == 0) {
+        //set to create contracts
+        return { type: common.bladeburnerActions.type.general, name: common.bladeburnerActions.general.inciteViolence }
+    }
+
+    //failsafe: just train
+    return { type: common.bladeburnerActions.type.general, name: common.bladeburnerActions.general.training }
+}
+*/
