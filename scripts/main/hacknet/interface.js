e38fa1bd9b988e1dde38c5836d17d204719112db
Joost <github@joostnieuweboer.nl>
2024-12-10 14:20:13 +0100

    Create interface.js
    Created interface

---
+++ scripts/main/hacknet/interface.js
@@ -0,0 +1,11 @@
+//import common for checking  if functionality is unlocked
+import * as common from "./common.js"
+
+//import the the needed namespaces
+import servers from "./servers.js"
+import hacknet from "./hacknet.js"
+
+//check which one to export (depending on availability)
+const exported_namespace = common.functionality_available(common.functionality.hacknet) ? hacknet : servers
+//export the specific value
+export default exported_namespace
