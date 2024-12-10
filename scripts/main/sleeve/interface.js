73f48015072286b6a06a1d44f09a55901f4e1204
Joost <github@joostnieuweboer.nl>
2024-12-10 14:21:41 +0100

    Update interface.js
    Fixed import paths

--- scripts/main/sleeve/interface.js
+++ scripts/main/sleeve/interface.js
@@ -2,8 +2,8 @@
 import * as common from "./common.js"
 
 //import the the needed namespaces
-import sleeve_dummy from "./sleeve_dummy.js"
-import sleeve from "./sleeve.js"
+import sleeve_dummy from "sleeve_dummy.js"
+import sleeve from "sleeve.js"
 
 //check which one to export (depending on availability)
 const exported_namespace = common.functionality_available(common.functionality.sleeve) ? sleeve : sleeve_dummy
