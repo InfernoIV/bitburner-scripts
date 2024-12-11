bec1cb512f13777b20525e0a439e3129fb6ce117
Joost <github@joostnieuweboer.nl>
2024-12-11 18:48:35 +0100

    Update interface.js
    Fixed paths

--- scripts/main/bladeburner/interface.js
+++ scripts/main/bladeburner/interface.js
@@ -1,9 +1,9 @@
 //import common for checking  if functionality is unlocked
-import * as common from "./common.js"
+import * as common from "scripts/common.js"
 
 //import the the needed namespaces
-import bladeburner_dummy from "bladeburner_dummy.js"
-import bladeburner from "bladeburner.js"
+import bladeburner_dummy from "./bladeburner_dummy.js"
+import bladeburner from "./bladeburner.js"
 
 //check which one to export (depending on availability)
 const exported_namespace = common.functionality_available(common.functionality.bladeburner) ? bladeburner : bladeburner_dummy
