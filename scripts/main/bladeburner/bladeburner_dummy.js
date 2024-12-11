53ccf007bcf46e29775cab1d4793c686468054fc
Joost <github@joostnieuweboer.nl>
2024-12-11 18:49:34 +0100

    Update bladeburner_dummy.js
    Fixed function calls

--- scripts/main/bladeburner/bladeburner_dummy.js
+++ scripts/main/bladeburner/bladeburner_dummy.js
@@ -1,8 +1,8 @@
 /*
 Dummy file to prevent RAM calculation costs if bladeburner is not unlocked
 */
-export function get_access(...) { return false }
-export function manage_action(...) { return }
-export function is_performing_black_op(...) { return false }
-export function has_completed_all_black_ops(...) { return false }
-export function update_ui(...) { return [],[] }
+export function get_access(ns) { return false }
+export function manage_action(ns) { return }
+export function is_performing_black_op(ns) { return false }
+export function has_completed_all_black_ops(ns) { return false }
+export function update_ui(ns) { return [],[] }
