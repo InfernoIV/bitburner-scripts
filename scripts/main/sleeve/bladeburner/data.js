54b6338b591f26a8fe62826955a2667b3277dc5e
Joost <github@joostnieuweboer.nl>
2024-12-16 14:14:17 +0100

    Update data.js
    Removed information

--- scripts/main/sleeve/bladeburner/data.js
+++ scripts/main/sleeve/bladeburner/data.js
@@ -1,11 +1,1 @@
-/**
- * Enum that describes the bladeburner actions that sleeves can take
- */
-export const bladeburner_actions = {
-    fieldAnalysis: "Field Analysis",
-    recruitment: "Recruitment",
-    diplomacy: "Diplomacy",
-    infiltratesynthoids: "Infiltrate synthoids",
-    supportmainsleeve: "Support main sleeve",
-    takeonContracts: "Take on Contracts",
-}
+
