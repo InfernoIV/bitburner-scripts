efdf121e5979949dec6f388347c6adfaa1da67d4
Joost <github@joostnieuweboer.nl>
2024-12-16 14:33:21 +0100

    Update bladeburner.js
    removed get_shock
    created action_list class to keep track of saved actions
    refactored determine_action

--- scripts/main/sleeve/bladeburner/bladeburner.js
+++ scripts/main/sleeve/bladeburner/bladeburner.js
@@ -11,53 +11,33 @@ import * as bladeburner from "../../bladeburner/bladeburner.js"
 
 /**
  * Function that determines the sleeve work for bladeburner
- * tries to assign bladeburner work from the lowest index, not checking stats
  * function should be executed before assigning normal work!
+ * try contracts & operations?
  * Cost: 8 GB
  *   ns.bladeburner.getActionEstimatedSuccessChance: 4 GB
  *   ns.bladeburner.getCityChaos: 4 GB
 **/
-export function manage_actions(ns, sleeve_actions) {
-    //check if we have enough chance to recruit, default to sleeve 0
-    if(ns.bladeburner.getActionEstimatedSuccessChance(data.bladeburner_actions.sleeve.recruitment, 0) >= config.bladeburner_success_chance_minimum_sleeve   {
-        //set sleeve 0 to recruit
-         sleeve_actions[0] = { type: data.bladeburner_actions.sleeve.recruitment }
-    }
-    
+export function determine_action(ns, index) {
+    //check if we have enough chance to recruit
+    if(ns.bladeburner.getActionEstimatedSuccessChance(data.bladeburner_actions.sleeve.recruitment, index) >= config.bladeburner_success_chance_minimum_sleeve   {
+        //set to recruit
+         return { type: data.activities.bladeburner, value: bladeburner.data.actions.sleeve.recruitment } //bladeburner action
+    }        
     //if action count is too low
-    if (bladeburner.bladeburner_get_lowest_action_count(ns) <= config.bladeburner_minimum_number_of_actions) {
-        //set index to 0
-        let index = 0
-        //if sleeve 0 is already doing work
-        if (sleeve_actions[index].type == data.bladeburner_actions.sleeve.recruitment) {
-            //set index to 1
-            index = 1
-        }
-        //set sleeve to work on this
-        sleeve_actions[index] = { type: data.bladeburner_actions.sleeve.infiltrate_synthoids }
+    else if (bladeburner.bladeburner_get_lowest_action_count(ns) <= config.bladeburner_minimum_number_of_actions) {
+        //set to infiltrate to raise action count
+        return { type: data.activities.infiltrate, value: bladeburner.data.actions.sleeve.infiltrate_synthoids} //specific type
     }
-
     //if city chaos is over threshold
-    if (ns.bladeburner.getCityChaos(bladeburner.bladeburner_get_best_city(ns)) > config.bladeburner_chaos_threshold) {
-        //set index to 0
-        let index = 0
-        //if sleeve 0 is already doing work
-        if (sleeve_actions[index].type == data.bladeburner_actions.sleeve.recruitment || sleeve_actions[index].type == data.bladeburner_actions.sleeve.infiltrate_synthoids) {
-            //up the index
-            index = 1
-            //check again
-            //if sleeve 1 is already doing work
-            if (sleeve_actions[index].type == data.bladeburner_actions.sleeve.recruitment || sleeve_actions[index].type == data.bladeburner_actions.sleeve.infiltrate_synthoids) {
-                //up the index
-                index = 2
-            }
-        }
-        //set sleeve to work on lowering chaos
-        sleeve_actions[index] = { type: data.bladeburner_actions.sleeve.diplomacy }
+    else if (ns.bladeburner.getCityChaos(bladeburner.bladeburner_get_best_city(ns)) > config.bladeburner_chaos_threshold) {
+        //lower chaos (todo: move sleeve???)
+        return { type: data.activities.bladeburner, value: bladeburner.data.actions.sleeve.diplomacy } //bladeburner action
     }
-    
-    //return the value back
-    return sleeve_actions
+    //cannot do (or allowed to) any bladeburner work
+    else {
+        //not allowed
+        return { type: "DENIED", value: " " }
+    }    
 }
 
 
@@ -70,13 +50,13 @@ export function manage_actions(ns, sleeve_actions) {
 **/
 export function execute_action(ns, sleeve_index, sleeve_action) {
     //check if we want to handle it
-    switch (sleeve_action.type) {
+    switch (sleeve_action.value) {
         //if the type is a blade burner action
-        case data.bladeburner_actions.sleeve.recruitment:
-        case data.bladeburner_actions.sleeve.infiltrate_synthoids: 
-        case data.bladeburner_actions.sleeve.diplomacy: 
+        case bladeburner.data.actions.sleeve.recruitment:
+        case bladeburner.data.actions.sleeve.infiltrate_synthoids: 
+        case bladeburner.data.actions.sleeve.diplomacy: 
             //set to bladeburner action
-            ns.sleeve.setToBladeburnerAction(index, sleeve_action.type)
+            ns.sleeve.setToBladeburnerAction(index, sleeve_action.value)
             //only handle bladeburner stuff
             break
             
@@ -86,53 +66,3 @@ export function execute_action(ns, sleeve_index, sleeve_action) {
             common.log(ns, 1, common.warning, "execute_action - uncaught condition: " + sleeve_index + " has action " + sleeve_action)
     }
 }
-
-
-/**
- * Function that checks the best work type for bladeburner for sleeve
- * Only perform 100% chance work, else we get shock
- * cannot lower chaos, requires to get city, which increases ram usage
- * @param {NS} ns
- * Cost: 0 GB
- */
-    /*
-function getBladeburnerActionForSleeve(ns, sleeveIndex) {
-    //set min chance = 100%
-    const bladeburner_success_chance_minimum = 1
-    //describe other actions
-    const common.sleeveBladeburnerActions = {
-        fieldAnalysis: "Field Analysis",
-        infiltratesynthoids: "Infiltrate synthoids",
-    }
-
-    //keep track of action count
-    let bladeburner_total_action_count = 0
-
-    //contracts
-    //for each contract
-    for (const operation in common.bladeburnerActions.contracts) {
-        //get contract information
-        const contract = common.bladeburnerActions.contracts[operation]
-        //get action count
-        const action_count = ns.bladeburner.getActionCountRemaining(common.bladeburnerActions.type.contracts, contract)
-        //get chance
-        const chance = ns.bladeburner.getActionEstimatedSuccessChance(common.bladeburnerActions.type.contracts, contract, sleeveIndex)
-        //if this action can be performed and we have enough chance
-        if ((action_count > 0) && (chance[0] >= bladeburner_success_chance_minimum)) {
-            //return this information
-            return { type: common.sleeveBladeburnerActions.takeonContracts, name: contract }
-        }
-        //add count to total actions available
-        bladeburner_total_action_count += action_count
-    }
-
-    //if no operations or contracts available
-    if (bladeburner_total_action_count == 0) {
-        //set to create contracts
-        return { type: common.bladeburnerActions.type.general, name: common.bladeburnerActions.general.inciteViolence }
-    }
-
-    //failsafe: just train
-    return { type: common.bladeburnerActions.type.general, name: common.bladeburnerActions.general.training }
-}
-*/
