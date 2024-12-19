0672d3544d42eee63d13e53554e094ad23f3ada8
Joost <github@joostnieuweboer.nl>
2024-12-19 07:53:19 +0100

    Update bladeburner.js
    Disabled team size due to ram constraints (adds 8 GB)

--- scripts/main/bladeburner/bladeburner.js
+++ scripts/main/bladeburner/bladeburner.js
@@ -344,9 +344,9 @@ function determine_action(ns) {
             //check if this is the black op that is to be done
             if (ns.bladeburner.getActionCountRemaining(data.actions.type.blackOps, black_op.name) > 0) {
                 //get team size first
-                const available_members = ns.bladeburner.getTeamSize()
+                //const available_members = ns.bladeburner.getTeamSize()
                 //set all members
-                ns.bladeburner.setTeamSize(data.actions.type.blackOps, black_op.name, available_members)
+                //ns.bladeburner.setTeamSize(data.actions.type.blackOps, black_op.name, available_members)
                 //get chance
                 const chance = ns.bladeburner.getActionEstimatedSuccessChance(data.actions.type.blackOps, black_op.name)
                 //check if we have enough rank and enough chance
@@ -421,6 +421,9 @@ function determine_action(ns) {
  * Function that clears the teams
  */
 function clear_teams(ns) {
+    //disabled
+    return
+    /*
     //for each black operations
     for (const activity in data.actions.blackOps) {
         //get black  operation information
@@ -434,5 +437,5 @@ function clear_teams(ns) {
         const operation = data.actions.operations[activity]
         //set size to 0
         ns.bladeburner.setTeamSize(data.actions.type.operations, operation, 0)
-    }
+    }*/
 }
